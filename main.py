c3b8ae5c07d8f9424d9b7a98cc18eab8da7720dc
Matthew Wilcox <mcwilcox2@gmail.com>
2025-09-26 01:40:39 +0200

    Player rotation and movement

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 import pygame
+from player import *
+from circleshape import *
 from constants import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    clock = pygame.time.Clock()
+    dt = 0
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
@@ -15,7 +22,10 @@ def main():
           if event.type == pygame.QUIT:
             return
         screen.fill('black')
+        player.update(dt)
+        player.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
